449da23b94defd585ea0614714851f30bff28e6b
sulenchy <sulaimanabiodun172@gmail.com>
2017-11-20 23:49:16 +0100

    Ft(signin-user-api-endpoint):adding signin-user-api-endpoint feature

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -79,24 +79,23 @@ export class UserSignin {
       if
      */
     const { email, password } = req.body;
-    if (!email || !password) {
-      return res.status(400).send({
-        status: 'Fail',
-        message: 'Please enter your username and password',
-      });
-    }
+    // if (!email || !password) {
+    //   return res.status(400).send({
+    //     status: 'Sign-in Error',
+    //     message: 'Please enter your username and password',
+    //   });
+    // }
     return Users // check the db if user has already signedup
       .findOne({
         where: {
           email,
-          password,
         },
       })
       .then((user) => {
         if (!user) { // returns an error if user has not signedup yet
           return res.status(400).send({
-            status: 'Fail',
-            err: 'User Not Found',
+            status: 'Sign-in Error',
+            err: 'User not found. Sign-in with correct data or signup as a new client',
           });
         }
         if (bcrypt.compareSync(password, user.password)) {
@@ -111,12 +110,12 @@ export class UserSignin {
           });
           return res.status(200).send({
             status: 'Success',
-            message: 'Token generation and signin successful',
+            message: 'Congratulation, you successfully signed-in into andevents',
             data: token,
           });
         }
         return res.status(400).send({
-          status: 'Fail',
+          status: 'Login Error',
           message: 'Incorrect Login Details supplied',
         });
       });
