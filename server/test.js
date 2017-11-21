04ef6cf4780e1c248cef49e30e69a47f56ef3f1f
sulenchy <sulaimanabiodun172@gmail.com>
2017-11-21 18:22:13 +0100

    Ch(setup-travis): Initializing the setup travis features

--- server/test.js
+++ server/test.js
@@ -1,3 +1,9 @@
+import chai from 'chai';
+import request from 'supertest';
+import app from './index';
+
+const assert = chai.assert;
+
 const users = [
 
 ];
@@ -9,4 +15,21 @@ const events = [
 const centers = [
 
 ];
+
+describe('Andevents API endpoint Test', () => {
+
+});
+
+describe('GET "/api/v1/home", to test server ', () => {
+  it('should respond with a 200 status code, status, and message', (done) => {
+    request(app)
+      .get('/api/v1/home')
+      .end((err, res) => {
+        assert.deepEqual(res.body.status, 'Success');
+        assert.deepEqual(res.body.message, 'Welcome to Andevents API Endpoint!');
+        assert.deepEqual(res.status, 200);
+        done();
+      });
+  });
+});
 
