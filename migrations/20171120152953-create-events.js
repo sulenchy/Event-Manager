2326d9a7f2f39719cdaa823aeb61d342cb85c5e1
sulenchy <sulaimanabiodun172@gmail.com>
2017-11-23 16:23:03 +0100

    Bg(Bg-travis-blocker):first attempt

---
+++ migrations/20171120152953-create-events.js
@@ -0,0 +1,66 @@
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+      return queryInterface.createTable('Events', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        title: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        description: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        event_type: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        estimated_attendees: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        event_date: {
+          type: Sequelize.DATEONLY,
+          allowNull: false,
+          unique: true,
+        },
+        lga: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        userId: {
+          type: Sequelize.INTEGER,
+          onDelete: 'CASCADE',
+          references: {
+            model: 'Users',
+            key: 'id',
+            as: 'userId',
+          },
+        },
+        centerId: {
+          type: Sequelize.INTEGER,
+          onDelete: 'CASCADE',
+          references: {
+            model: 'Centers',
+            key: 'id',
+            as: 'centerId',
+          },
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+      }); 
+    },
+    down: (queryInterface /* , Sequelize */) => {
+      return queryInterface.dropTable('Events');
+    }
+  };
